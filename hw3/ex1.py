b2045abf7ff8726513195ae23e438eb30358922e
Sasha <pisarevasas@yandex.ru>
2023-04-22 22:58:45 +0300

    ex1

--- hw3/ex1.py
+++ hw3/ex1.py
@@ -3,6 +3,11 @@
 # 6 –> 1 1 2 3 5 8 
 
 def zadacha1():
+    num = [0, 1]
+    a = int(input('Введите число: '))
+    for i in range(2, a):
+        num.append(num[i - 2] + num[i - 1])
+    print(num)    
+    
 zadacha1()
 
-# цикл 2:05
